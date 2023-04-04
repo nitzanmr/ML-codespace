bfc0b18ce6525f818426ea24c7a7f1dbb0062738
nitzanmr <81086233+nitzanmr@users.noreply.github.com>
2023-04-04 12:06:21 +0300

    Add files via upload

---
+++ default.py
@@ -0,0 +1,10 @@
+# Created on iPad.
+import numpy as np
+import matplotlib as plt
+import pandas as pd
+
+# Read the CSV file
+airbnb_data = pd.read_csv("cancer_data.csv")
+
+# View the first 5 rows
+airbnb_data.head()
