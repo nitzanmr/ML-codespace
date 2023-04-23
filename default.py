7cc6e3f096418d0c781bf775097039d954ae9e2c
nitzanmr <81086233+nitzanmr@users.noreply.github.com>
2023-04-23 16:25:07 +0000

    finshed results

--- default.py
+++ default.py
@@ -1,10 +1,13 @@
 import ex1
 import numpy as np
 def __main__():
-  matrix = np.ones((5,5))
-  for i in range(5):
-    for j in range(5):
-      matrix[i][j] = i * 5 + j
+
+
+  # matrix = np.ones((5,5))
+  # for i in range(5):
+  #   for j in range(5):
+  #     matrix[i][j] = i * 5 + j
+  matrix = np.loadtxt(open("cancer_data.csv", "rb"), delimiter=",")
   matrix,u,v = ex1.fixData(matrix)
   x = matrix[:, :-1]
   x = np.hstack((np.ones((x.shape[0],1)), x))
@@ -15,11 +18,11 @@ def __main__():
   # print(x)
   # print('\n')
   theta = np.zeros(x.shape[1])
-  
+
   # print(theta.reshape(5,1))
   # theta = theta.reshape(5,1)
   # print(theta.shape)
-  theta = ex1.GD(x,y,theta,alpha=0.05)
+  theta = ex1.Adam(x,y,theta,alpha=0.01)
   print(theta)
   print(y)
 
